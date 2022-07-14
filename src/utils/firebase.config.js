f142cad05b005b4e0db81025f1bf7309473c89e2
Rinaldi Richard <rinaldi.richatd@gmail.com>
2022-07-14 11:56:52 +0200

    first commit

---
+++ src/utils/firebase.config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyCHkauAH_BkWDbzsIrfWVL70kMwXRoq64U",
+  authDomain: "react-firebase-redux-b1535.firebaseapp.com",
+  projectId: "react-firebase-redux-b1535",
+  storageBucket: "react-firebase-redux-b1535.appspot.com",
+  messagingSenderId: "247185200652",
+  appId: "1:247185200652:web:95891fff8bf768f601c029",
+});
+
+export const auth = app.auth();
+export default app;
