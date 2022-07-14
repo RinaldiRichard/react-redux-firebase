5917121982dfe9782a7e7fb6660c1edc9ce6cbc0
Rinaldi Richard <rinaldi.richatd@gmail.com>
2022-07-14 12:26:50 +0200

    ajout formulaire inscription firebase

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { auth } from "../utils/firebase.config";
 
 const SignUp = () => {
-    return (
-        <div>
-            <h1>Sign Up</h1>
-        </div>
-    );
+  const registerEmail = useRef();
+  const registerPassword = useRef();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    try {
+      auth.createUserWithEmailAndPassword(
+        registerEmail.current.value,
+        registerPassword.current.value
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+    console.log(registerEmail.current.value, registerPassword.current.value);
+  };
+
+  return (
+    <div className="signup-container">
+      <div className="signup">
+        <h3>S'inscrire</h3>
+        <form onSubmit={(e) => handleRegister(e)}>
+          <input type="text" placeholder="Pseudo" required />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            ref={registerEmail}
+          />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            required
+            ref={registerPassword}
+          />
+          <input type="submit" value="Valider inscription" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
