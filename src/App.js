01928d5d2107d4fd1fa17bd880cdd88cb7492fde
Rinaldi Richard <rinaldi.richatd@gmail.com>
2022-07-14 14:57:20 +0200

    ajout logique affichage selon authentification et gestion connection user via firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ConnectModal from "./components/ConnectModal";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./utils/firebase.config";
+import CreatePost from "./components/CreatePost";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
   return (
     <div>
       <div className="app-header">
-        <ConnectModal />
+        {user && (
+          <div className="user-infos">
+            <span>{user?.displayName[0]} </span>
+            <h4>{user?.displayName}</h4>
+            <button>
+              <i className="fa-solid fa-arrow-right-from-bracket"></i>
+            </button>
+          </div>
+        )}
+        {user ? <CreatePost /> : <ConnectModal />}
       </div>
+
       <div className="posts-container"></div>
     </div>
   );
