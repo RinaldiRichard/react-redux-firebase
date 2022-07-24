f0cd9a618a6a094280a5e4eab980184175366e7f
Rinaldi Richard <rinaldi.richatd@gmail.com>
2022-07-24 20:04:34 +0200

    commit

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import { getFirestore } from "@firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCHkauAH_BkWDbzsIrfWVL70kMwXRoq64U",
+  authDomain: "react-firebase-redux-b1535.firebaseapp.com",
+  projectId: "react-firebase-redux-b1535",
+  storageBucket: "react-firebase-redux-b1535.appspot.com",
+  messagingSenderId: "247185200652",
+  appId: "1:247185200652:web:95891fff8bf768f601c029",
 });
 
 export const auth = app.auth();
