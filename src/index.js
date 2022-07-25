4bb09ca65eda891bfec6898167c92381f581b59c
Rinaldi Richard <rinaldi.richatd@gmail.com>
2022-07-25 11:51:00 +0200

    modif dossier style pour futur responsive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.scss";
+import "./style/style.css";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
